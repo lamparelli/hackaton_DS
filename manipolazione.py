8198dec7b40934509dddd9304763c2680291e8b2
Marco <marco.sciacovelli99@gmail.com>
2021-12-13 18:24:08 +0100

    eliminazione colonne non utili

--- manipolazione.py
+++ manipolazione.py
@@ -15,7 +15,15 @@ def rinominaColonne(stb_data):
 def manipolaColonne(stb_data):
     print("todo, manipolazione colonne")
 
+def eliminaColonne(stb_data):
+    pd.set_option("max_rows", None)
+    cols = pd.Series(stb_data.columns)
+    #tutte le colonne da eliminare in quanto hanno _split
+    colsDaEliminare = cols[(cols.str.contains("_split") & (~cols.str.contains("aamp_abr_bw_split") & (~cols.str.contains("ap_info_split"))))] 
+    #elimino le colonne
+    stb_data.drop(columns=colsDaEliminare, inplace=True) 
 
 def preparaDati(stb_data):
     rinominaColonne(stb_data)
+    eliminaColonne(stb_data)
     manipolaColonne(stb_data)
