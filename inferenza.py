df4d1f789f786163401f25f02b195341d2443259
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2021-12-15 12:32:35 +0100

    init logica inferenza con MBA

---
+++ inferenza.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import lettura_dati
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+
+def getDatiEventiWideToLong(stb_data):
+    stb_data2 = stb_data.copy()
+    stb_data2["id"] = stb_data["mac"] + "_" + stb_data["time"]
+    stb_data_long = pd.melt(stb_data2, id_vars="id", value_vars=lettura_dati.getEventiCols(stb_data2))
+    stb_data_long.rename(columns={"variable": "event", "value": "event_count"}, inplace=True)
+    return stb_data_long
+
+def getDatiBasket(stb_data):
+    stb_data_long = getDatiEventiWideToLong(stb_data)
+    basket = pd.pivot_table(data=stb_data_long, index='id',columns='event',values='event_count', aggfunc='sum',fill_value=0)
+    return basket
+
+def convertiDatiBasketInInfoPresenza(datoBasket):
+    if datoBasket > 0:
+        return 1
+    else:
+        return 0
+
+def getCausaliOsm(stb_data):
+    datiBasket = getDatiBasket(stb_data)
+    datiPresenza = datiBasket.applymap(convertiDatiBasketInInfoPresenza)
+    setFrequenti = apriori(datiPresenza, min_support=0.05, use_colnames=True)
+    regoleAssociative = association_rules(setFrequenti, metric="lift", min_threshold=2)
+    return regoleAssociative.sort_values("lift", ascending=False)
+
+
