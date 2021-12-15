345242346b33b941aae9e6398df10172cc7beaa3
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2021-12-15 17:46:45 +0100

    aggiunta possibilita lettura id insieme a categorie colonne

--- lettura_dati.py
+++ lettura_dati.py
@@ -1,15 +1,27 @@
-def getEventiCols(stb_data):
+def getEventiCols(stb_data, getIdCols = False):
     cols = stb_data.columns
-    return cols[cols.str.contains("err|warn|info")]
+    if (getIdCols):
+        return cols[cols.str.contains("err|warn|info|mac|time")]
+    else:
+        return cols[cols.str.contains("err|warn|info")]
 
-def getEventiBackgroundCols(stb_data):
+def getEventiBackgroundCols(stb_data, getIdCols = False):
     cols = stb_data.columns
-    return cols[~cols.str.contains("osm") & (cols.str.contains("err|warn|info"))]
+    if (getIdCols):
+        return cols[~cols.str.contains("osm") & (cols.str.contains("err|warn|info|mac|time"))]
+    else:
+        return cols[~cols.str.contains("osm") & (cols.str.contains("err|warn|info"))]
 
-def getEventiOsmCols(stb_data):
+def getEventiOsmCols(stb_data, getIdCols = False):
     cols = stb_data.columns
-    return cols[cols.str.contains("osm")]
+    if (getIdCols):
+        return cols[cols.str.contains("osm|mac|time")]
+    else:
+        return cols[cols.str.contains("osm")]
 
-def getStatiCols(stb_data):
+def getStatiCols(stb_data, getIdCols = False):
     cols = stb_data.columns
-    return cols[~cols.str.contains("err|warn|info") & ~cols.str.contains("mac|time")]
+    if (getIdCols):
+        return cols[~cols.str.contains("err|warn|info") | cols.str.contains("mac|time")]
+    else:
+        return cols[~cols.str.contains("err|warn|info")]
